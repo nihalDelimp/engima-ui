39115fc88909c0220c8e14af89099f1686a2bb8e
nihalTypeApps <nihal@type-apps.com>
2022-03-22 12:43:04 +0530

    Image error

--- src/Containers/Contacts/index.js
+++ src/Containers/Contacts/index.js
@@ -1,72 +1,76 @@
 import React from "react";
+import contImg from "../../assets/img/Ellipse.png"
+import contImg2 from "../../assets/img/Ellipse (1).png"
+import contImg3 from "../../assets/img/Ellipse4.png"
+import contImg4 from "../../assets/img/Ellipse (2).png"
 
-const Contacts = () =>{
-    return(
+const Contacts = () => {
+    return (
         <div className="col-xl-3 col-lg-9 card-group space-right space-left p-0">
-        <div className="card gedf-card">
-            <div className="card-body left-menu">
-                <h5 className="card-title cont-hed text-muted">5 Contacts</h5>
-                
-                <div className="d-flex justify-content-between align-items-center contact-padding">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="mr-2">
-                            <img className="rounded-circle" width="45" src="img/Ellipse.png" alt=""/>
+            <div className="card gedf-card">
+                <div className="card-body left-menu">
+                    <h5 className="card-title cont-hed text-muted">5 Contacts</h5>
+
+                    <div className="d-flex justify-content-between align-items-center contact-padding">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="mr-2">
+                                <img className="rounded-circle" width="45" src={contImg} alt="" />
+                            </div>
+                            <div className="ml-2">
+                                <div className="h7 font-16">Rebecca Davis</div>
+                            </div>
                         </div>
-                        <div className="ml-2">
-                            <div className="h7 font-16">Rebecca Davis</div>
+
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center contact-padding">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="mr-2">
+                                <img className="rounded-circle" width="45" src={contImg4} alt="" />
+                            </div>
+                            <div className="ml-2">
+                                <div className="h7 font-16">Sarah Smyth</div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center contact-padding">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="mr-2">
+                                <img className="rounded-circle" width="45" src={contImg2} alt="" />
+                            </div>
+                            <div className="ml-2">
+                                <div className="h7 font-16">Dayton Gueverra</div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div className="d-flex justify-content-between align-items-center contact-padding">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="mr-2">
+                                <img className="rounded-circle" width="45" src={contImg3} alt="" />
+                            </div>
+                            <div className="ml-2">
+                                <div className="h7 font-16">Danny</div>
+                            </div>
                         </div>
                     </div>
-                
-                </div>
-                 <div className="d-flex justify-content-between align-items-center contact-padding">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="mr-2">
-                            <img className="rounded-circle" width="45" src="img/Ellipse4.png" alt=""/>
-                        </div>
-                        <div className="ml-2">
-                            <div className="h7 font-16">Sarah Smyth</div>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-                <div className="d-flex justify-content-between align-items-center contact-padding">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="mr-2">
-                            <img className="rounded-circle" width="45" src="img/Ellipse (1).png" alt=""/>
-                        </div>
-                        <div className="ml-2">
-                            <div className="h7 font-16">Dayton Gueverra</div>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-                
-                <div className="d-flex justify-content-between align-items-center contact-padding">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="mr-2">
-                            <img className="rounded-circle" width="45" src="img/Ellipse (2).png" alt=""/>
-                        </div>
-                        <div className="ml-2">
-                            <div className="h7 font-16">Danny</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="d-flex justify-content-between align-items-center contact-padding">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="mr-2">
-                            <img className="rounded-circle" width="45" src="img/Ellipse (3).png" alt=""/>
-                        </div>
-                        <div className="ml-2">
-                            <div className="h7 font-16">Jacob</div>
+                    <div className="d-flex justify-content-between align-items-center contact-padding">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="mr-2">
+                                <img className="rounded-circle" width="45" src={contImg} alt="" />
+                            </div>
+                            <div className="ml-2">
+                                <div className="h7 font-16">Jacob</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
-       
-    </div>
     )
 }
 export default Contacts
--- src/Containers/Dashboard/index.js
+++ src/Containers/Dashboard/index.js
@@ -11,135 +11,135 @@ const Dashboard = () => {
 
     return (
         <>
-        <div className="col-xl-6 col-lg-9 space-main p-0">
-            <div className="card-body p-0 active-sec">
-                <h5 className="card-title">Account status:  <span>Active</span></h5>
-            </div>
-            <div className="card-header p-0 border-0 bg-white top-sec">
-                <div className="d-flex justify-content-between align-items-center qr-sec">
-                    <div className="col-lg-8 d-flex gapfor_pro_q profile-sec" >
-                        <div className="profile_logo">
-                            <img src= {profileImg}/>
-                        </div>
-                        <div className="">
-                        <h3>{user.username}</h3>
-                       <p><span><b> {user.enigmaId}</b></span></p>
-                            {/* <p><span>Johndoe#</span> <b>123457890 </b></p>  */}
-                            <a href="#" className="btn btn-primary">EDIT ACCOUNT PROFILE</a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 qr-code-sec">
-                        <img src= {qr_code} alt="" className="img-rounded" />
-                    </div>
+            <div className="col-xl-6 col-lg-9 space-main p-0">
+                <div className="card-body p-0 active-sec">
+                    <h5 className="card-title">Account status:  <span>Active</span></h5>
                 </div>
-            </div>
-            <div className="card gedf-card mt-4 border-0">
-                <div className="card-header border-0 bg-white pb-0 pt-4">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="ml-2">
-                                <div className="recent-head h5 m-0">Recent Activities</div>
+                <div className="card-header p-0 border-0 bg-white top-sec">
+                    <div className="d-flex justify-content-between align-items-center qr-sec">
+                        <div className="col-lg-8 d-flex gapfor_pro_q profile-sec" >
+                            <div className="profile_logo">
+                                <img src={profileImg} />
+                            </div>
+                            <div className="">
+                                <h3>{user.username}</h3>
+                                <p><span><b> {user.enigmaId}</b></span></p>
+                                {/* <p><span>Johndoe#</span> <b>123457890 </b></p>  */}
+                                <a href="#" className="btn btn-primary">EDIT ACCOUNT PROFILE</a>
                             </div>
                         </div>
-                        <div>
-                            <div className="dropdown">
+                        <div className="col-lg-4 qr-code-sec">
+                            <img src={qr_code} alt="" className="img-rounded" />
+                        </div>
+                    </div>
+                </div>
+                <div className="card gedf-card mt-4 border-0">
+                    <div className="card-header border-0 bg-white pb-0 pt-4">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="d-flex justify-content-between align-items-center">
                                 <div className="ml-2">
-                                    <div className="p m-0 font-16 grey-color"><i className="fa fa-calendar-o pr-2" aria-hidden="true"></i>01 September - Today<i className="fa fa-angle-down pl-2" aria-hidden="true"></i>
+                                    <div className="recent-head h5 m-0">Recent Activities</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="dropdown">
+                                    <div className="ml-2">
+                                        <div className="p m-0 font-16 grey-color"><i className="fa fa-calendar-o pr-2" aria-hidden="true"></i>01 September - Today<i className="fa fa-angle-down pl-2" aria-hidden="true"></i>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="card border-0">
-                    <div className="card-body">
-                        <table className="table">
-                            <tbody>
-                                <tr data-status="pagado">
-                                    <td style={{ border: "0" }}>
-                                        <div className="media recent-sec">
+                    <div className="card border-0">
+                        <div className="card-body">
+                            <table className="table">
+                                <tbody>
+                                    <tr data-status="pagado">
+                                        <td style={{ border: "0" }}>
+                                            <div className="media recent-sec">
 
-                                            <img src="img/Ellipse (1).png" className="media-photo" />
+                                                <img src="img/Ellipse (1).png" className="media-photo" />
 
-                                            <div className="media-body font-16">
-                                                <span className="media-meta pull-right">Last week</span>
-                                                <p className="summary pl-3 grey-color">You update profile picture</p>
+                                                <div className="media-body font-16">
+                                                    <span className="media-meta pull-right">Last week</span>
+                                                    <p className="summary pl-3 grey-color">You update profile picture</p>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                </tr>
-                                <tr data-status="pagado">
+                                    </tr>
+                                    <tr data-status="pagado">
 
-                                    <td style={{ border: "0" }}>
-                                        <div className="media recent-sec font-16">
+                                        <td style={{ border: "0" }}>
+                                            <div className="media recent-sec font-16">
 
-                                            <img src="img/Ellipse (1).png" className="media-photo" />
+                                                <img src="img/Ellipse (1).png" className="media-photo" />
 
-                                            <div className="media-body">
-                                                <span className="media-meta pull-right">Last week</span>
-                                                <p className="summary pl-3 grey-color">You update profile picture</p>
+                                                <div className="media-body">
+                                                    <span className="media-meta pull-right">Last week</span>
+                                                    <p className="summary pl-3 grey-color">You update profile picture</p>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                </tr>
-                                <tr data-status="pagado">
+                                    </tr>
+                                    <tr data-status="pagado">
 
-                                    <td style={{ border: "0" }}>
-                                        <div className="media recent-sec font-16">
+                                        <td style={{ border: "0" }}>
+                                            <div className="media recent-sec font-16">
 
-                                            <img src="img/Ellipse (1).png" className="media-photo" />
+                                                <img src="img/Ellipse (1).png" className="media-photo" />
 
-                                            <div className="media-body">
-                                                <span className="media-meta pull-right">Last week</span>
-                                                <p className="summary pl-3 grey-color">You update profile picture</p>
+                                                <div className="media-body">
+                                                    <span className="media-meta pull-right">Last week</span>
+                                                    <p className="summary pl-3 grey-color">You update profile picture</p>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                </tr>
-                                <tr data-status="pagado">
+                                    </tr>
+                                    <tr data-status="pagado">
 
-                                    <td style={{ border: "0" }}>
-                                        <div className="media recent-sec font-16">
+                                        <td style={{ border: "0" }}>
+                                            <div className="media recent-sec font-16">
 
-                                            <img src="img/Ellipse (1).png" className="media-photo" />
+                                                <img src="img/Ellipse (1).png" className="media-photo" />
 
-                                            <div className="media-body">
-                                                <span className="media-meta pull-right">Last week</span>
-                                                <p className="summary pl-3 grey-color">You update profile picture</p>
+                                                <div className="media-body">
+                                                    <span className="media-meta pull-right">Last week</span>
+                                                    <p className="summary pl-3 grey-color">You update profile picture</p>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                </tr>
-                                <tr data-status="pagado">
+                                    </tr>
+                                    <tr data-status="pagado">
 
-                                    <td style={{ border: "0" }}>
-                                        <div className="media recent-sec font-16">
+                                        <td style={{ border: "0" }}>
+                                            <div className="media recent-sec font-16">
 
-                                            <img src="img/Ellipse (1).png" className="media-photo" />
+                                                <img src="img/Ellipse (1).png" className="media-photo" />
 
-                                            <div className="media-body">
-                                                <span className="media-meta pull-right">Last week</span>
-                                                <p className="summary pl-3 grey-color">You update profile picture</p>
+                                                <div className="media-body">
+                                                    <span className="media-meta pull-right">Last week</span>
+                                                    <p className="summary pl-3 grey-color">You update profile picture</p>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Contacts/>
+            <Contacts />
         </>
     )
 }
