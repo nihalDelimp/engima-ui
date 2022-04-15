e3ab3578136a62012aaa9ca92aa5982b2d01549a
nihalTypeApps <nihal@type-apps.com>
2022-04-15 17:18:35 +0530

    UseRef Hook

--- src/Containers/Dashboard/index.js
+++ src/Containers/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import Contacts from "../Contacts"
 import { useSelector } from "react-redux"
 import IsloggedinHOC from '../../Components/IsLoggedinHOC'
