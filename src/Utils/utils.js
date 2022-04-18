8aa1b71b154269a739479097a4ddc8e9df17e1e8
nihalTypeApps <nihal@type-apps.com>
2022-04-18 08:18:45 +0530

    Heroku app

---
+++ src/Utils/utils.js
@@ -0,0 +1,5 @@
+
+
+export const host = "https://enigma-app-backend.herokuapp.com";
+export const sendMessageRoute = `${host}/message/add-message`;
+export const recieveMessageRoute = `${host}/message/get-message`;
